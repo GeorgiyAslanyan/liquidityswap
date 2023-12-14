209344e66c44dd66c8d6bfb09921d947847f4d35
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-12-14 22:09:47 +0500

    block fix

--- components/Description.jsx
+++ components/Description.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const Description = () => {
-
   return (
     <div className="min-h-screen flex items-center justify-center min-w-screen pb-[70px]">
       <div className="mx-10 flex flex-col gap-[51px]">
@@ -13,68 +12,64 @@ const Description = () => {
         </motion.h1>
         <div className="grid  grid-cols-3 gap-10">
           <motion.div
-            className="rounded-3xl flex flex-col justify-between bg-[#3c3c3c] p-5 gap-5"
+            className="rounded-3xl flex flex-col bg-[#3c3c3c] p-5 gap-5"
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0, duration: 0.4 }}>
+            <p className="text-[#EAC57E] bg-[#1F2123] px-4 py-2 rounded-full text-[18px]  w-fit">
+              Immediate Access
+            </p>{" "}
             <p className="text-[16px]">
-              <span className="text-gray-300 mb-6 bg-[#1F2123] px-3 py-2 rounded-full text-[14px]">
-                Immediate Access
-              </span>{" "}
-              <br /> <br /> Our service offers a unique solution that allows
-              project creators to access their locked ETH without waiting for
-              lengthy lock-in periods to expire. This ensures that innovation
-              never has to take a back seat due to locked funds.
+              Our service offers a unique solution that allows project creators
+              to access their locked ETH without waiting for lengthy lock-in
+              periods to expire. This ensures that innovation never has to take
+              a back seat due to locked funds.
             </p>
           </motion.div>
           <motion.div
-            className="rounded-3xl flex flex-col justify-between gap-5 bg-[#3c3c3c] p-5"
+            className="rounded-3xl flex flex-col gap-5 bg-[#3c3c3c] p-5"
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0, duration: 0.4 }}>
+            <p className="text-[#EAC57E] bg-[#1F2123] px-4 py-2 rounded-full text-[18px]  w-fit">
+              Maximize Your Ethereum&apos;s Potential
+            </p>
             <p className="text-[16px]">
-              <span className="text-gray-300 mb-6  bg-[#1F2123] px-3 py-2 rounded-full text-[14px]">
-                Maximize Your Ethereum&apos;s Potential
-              </span>
-              <br />
-              <br />
               With our service, your Ethereum isn&apos;t just sitting idle.
               It&apos;s an active asset, primed to fund your next venture or
               idea.
             </p>
           </motion.div>
           <motion.div
-            className="rounded-3xl gap-5 flex flex-col justify-between bg-[#3c3c3c] p-5"
+            className="rounded-3xl gap-5 flex flex-col bg-[#3c3c3c] p-5"
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0, duration: 0.4 }}>
+            <p className="text-[#EAC57E] bg-[#1F2123] px-4 py-2 rounded-full text-[18px]  w-fit">
+            Safety First
+            </p>
             <p className="text-[16px]">
-              <span className="text-gray-300 mb-6 bg-[#1F2123] px-3 py-2 rounded-full text-[14px]">
-                Global Operation, Personalized Experience
-              </span>
-              <br />
-              <br />
-              Our service is designed for global accessibility, ensuring that no
+              In collaboration with Kleros, we guarantee the utmost security for
+              every transaction. Kleros a renowned decentralized arbitration
+              service, ensures that every swap is executed with precision and
+              security in mind.
+            </p>
+          </motion.div>
+          <motion.div
+            className="rounded-3xl gap-5 col-span-3 flex flex-col bg-[#3c3c3c] p-5"
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0, duration: 0.4 }}>
+            <p className="text-[#EAC57E] bg-[#1F2123] px-4 py-2 rounded-full text-[18px] w-fit">
+            Global Operation, Personalized Experience
+            </p>
+            <p className="text-[16px]">
+            Our service is designed for global accessibility, ensuring that no
               matter where you are, you can unlock your liquidity. Plus, we
               ensure a personalized and confidential experience, understanding
               the unique needs and privacy concerns of each client.
             </p>
           </motion.div>
-          <motion.p
-            className="rounded-3xl col-span-3 text-[16px] bg-[#3c3c3c] p-5"
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0, duration: 0.4 }}>
-            <span className="text-gray-300 mb-6 bg-[#1F2123] px-3 py-2 rounded-full text-[14px]">
-              Safety First
-            </span>
-            <br />
-            <br />
-            In collaboration with Kleros, we guarantee the utmost security for
-            every transaction. Kleros a renowned decentralized arbitration
-            service, ensures that every swap is executed with precision and
-            security in mind.
-          </motion.p>
         </div>
 
         <div className="col-span-3 text-[16px] flex gap-5 items-center justify-between w-full">
