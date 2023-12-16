b43396751ad1c2d6a574ff55d279e7bfb17fbab2
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-12-16 16:25:27 +0500

    commit micro changes

--- components/Cookies.jsx
+++ components/Cookies.jsx
@@ -6,25 +6,15 @@ const Cookies = () => {
   return (
     <>
       {cookie && (
-        <div className="fixed  bg-black px-3 md:px-10 py-3 md:py-5 bottom-0 w-screen">
+        <div className="fixed backdrop-blur-lg bg-black/50 px-3 md:px-10 py-3 md:py-5 bottom-0 w-screen">
           <div className="flex flex-col md:flex-row gap-3 md:gap-10 justify-between items-center">
-            <p className="text-sm hidden sm:block">
-              We use cookies and similar technologies to help personalize
-              content, tailor and measure ads, and provide a better experience.
-              <br />
-              By clicking accept, you agree to this, as outlined in our Cookie
-              Policy.
-            </p>
-            <p className="text-sm block sm:hidden">
-              We use cookies and similar technologies.
-              <br />
-              By clicking accept, you agree to this, as outlined in our Cookie
-              Policy.
+            <p className="text-sm">
+            This website uses cookies to offer you a better browsing experience. <br /> Find out more on how we use cookies.
             </p>
             <button
               onClick={() => setCookie(false)}
               className="text-sm w-full sm:w-fit items-center rounded-2xl whitespace-nowrap bg-[#1F2123] hover:bg-[#36393c] duration-300 ease-in-out transition-all text-white py-2 px-4">
-              Accept cookies
+              ACCEPT
             </button>
           </div>
         </div>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -21,23 +21,24 @@ const arr = [
 
 const Hero = () => {
   return (
-    <div className="bg-[url('/background.svg')] bg-234 md:bg-contain min-h-screen min-w-screen pb-[70px] rounded-2xl sm:rounded-3xl border border-[#EAC57E] mt-[72px] md:mt-5 lg:mt-10 mx-3 md:mx-5 lg:mx-10 relative ">
-      <div className="absolute pointer-events-none rotate-180 w-full flex items-center justify-center left-0 top-[90px]">
-            <div className="w-[90%] relative">
-              <LinesTop />
-              <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full z-1">
-                <div className=" ">
-                  <Image
-                    width={600}
-                    height={300}
-                    className="w-full opacity-20"
-                    alt="fade"
-                    src={"/fade.svg"}
-                  />
-                </div>
-              </div>
+    <div className="bg-[url('/background.svg')] bg-234 md:bg-contain min-h-screen min-w-screen rounded-2xl pb-[60px] xl:pb-0
+    sm:rounded-3xl border border-[#EAC57E] mt-[72px] md:mt-5 lg:mt-10 mx-3 md:mx-5 lg:mx-10 relative ">
+      <div className="absolute pointer-events-none rotate-180 w-full flex items-center justify-center left-0 top-[70px] md:top-[90px]">
+        <div className="w-[90%] relative">
+          <LinesTop />
+          <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full z-1">
+            <div className=" ">
+              <Image
+                width={600}
+                height={300}
+                className="w-full opacity-20"
+                alt="fade"
+                src={"/fade.svg"}
+              />
             </div>
           </div>
+        </div>
+      </div>
       <div className="flex flex-col justify-center items-center">
         <motion.div
           initial={{ opacity: 0, y: -20 }}
@@ -48,6 +49,17 @@ const Hero = () => {
           <Image src={"/logo.svg"} alt="logo" width={41} height={40} />
           <p className="">ØxLiquidity</p>
         </motion.div>
+        <motion.img
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 0.4, duration: 0.4 }}
+          viewport={{ once: true }}
+          width="100%"
+          height="100%"
+          src="/clock_alpha.gif"
+          className="pointer-events-none block md:hidden mt-12 object-cover md:object-contain h-[350px] md:h-full"
+          alt="Video tooltip on hover."
+        />
         <motion.video
           initial={{ opacity: 0, scale: 0.5 }}
           whileInView={{ opacity: 1, scale: 1 }}
@@ -55,7 +67,7 @@ const Hero = () => {
           viewport={{ once: true }}
           width="100%"
           height="100%"
-          className="pointer-events-none mt-8 object-cover md:object-contain h-[350px] md:h-full"
+          className="pointer-events-none hidden md:block mt-8 object-cover md:object-contain h-[350px] md:h-full"
           title="Video tooltip on hover."
           poster="/pre.png"
           muted
@@ -64,7 +76,7 @@ const Hero = () => {
           autoPlay>
           <source src="/clock_alpha.webm" type="video/webm" />
         </motion.video>
-        <div className="flex text-center flex-col gap-7 -mt-12">
+        <div className="flex text-center flex-col gap-7 -mt-8 md:-mt-12">
           <motion.p>
             <span className="text-[#EAC57E]">Do not wait</span> until the end of
             the liquidity lock-in period;
@@ -75,12 +87,13 @@ const Hero = () => {
           </motion.p>
           <motion.p>Let your ETH work for you!</motion.p>
           <motion.a
-            href="https://t.me/LPSwapBot"
-            target="_blank"
-            className="flex justify-center"
-            rel="noopener noreferrer">
-            <button className="items-center w-[90%] text-base lg:text-xl justify-center flex hover:gap-4 gap-2 sm:w-fit rounded-2xl bg-[#1F2123] hover:bg-[#36393c] duration-300 ease-in-out transition-all text-white py-3 px-5 h-full">
-              Apply now <svg
+              href="https://t.me/LPSwapBot"
+              target="_blank"
+              className="flex self-center justify-center w-fit"
+              rel="noopener noreferrer">
+              <button className="items-center text-base min-w-[188px] lg:text-xl whitespace-nowrap justify-center w-fit flex hover:gap-4 gap-2 sm:w-fit rounded-2xl bg-[#1F2123] hover:bg-[#36393c] duration-300 ease-in-out transition-all text-white py-3 px-5 h-full">
+                Apply now{" "}
+                <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -93,32 +106,26 @@ const Hero = () => {
                     d="M8.25 4.5l7.5 7.5-7.5 7.5"
                   />
                 </svg>
-            </button>
-          </motion.a>
+              </button>
+            </motion.a>
         </div>
         <div className="relative w-[90%] pointer-events-none xl:-translate-y-14">
-            <div>
-              <Lines />
-            </div>
-            <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full z-1">
-              <div className="w-[100px] ">
-                <InfinitySign />
-              </div>
-            </div>
-            <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full z-1">
-              <div className=" ">
-                <Image
-                  width={600}
-                  height={300}
-                  className="w-full opacity-30"
-                  alt="fade"
-                  src={"/fade.svg"}
-                />
-              </div>
+          <div>
+            <Lines />
+          </div>
+          <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full z-1">
+            <div className=" ">
+              <Image
+                width={600}
+                height={300}
+                className="w-full opacity-30"
+                alt="fade"
+                src={"/fade.svg"}
+              />
             </div>
           </div>
+        </div>
         <div className="flex flex-col gap-6 w-full px-5 md:px-14 xl:-translate-y-24">
-          
           <motion.p
             className="text-center"
             initial={{ opacity: 0 }}
@@ -129,7 +136,7 @@ const Hero = () => {
             Finance
           </motion.p>
 
-          <p className="text-justify text-lg lg:text-xl">
+          <p className="text-start lg:text-justify text-lg lg:text-xl">
             Launched in September 2023, 0xLiquidity emerged as a new protocol in
             the DeFi space, revolutionizing how liquidity is managed and
             utilized. At its core, 0xLiquidity provides a unique service that
@@ -138,8 +145,8 @@ const Hero = () => {
             projects. This allows devs to create more, invest more, and leverage
             their abilities to profit more.
           </p>
-          
-          <p className="text-justify text-lg lg:text-xl">
+
+          <p className="text-start lg:text-justify text-lg lg:text-xl">
             Over time, the protocol has not only established its effectiveness
             in meeting market demands, but has also robustly validated the
             security of its solutions. This success marks a pivotal moment for
@@ -171,7 +178,8 @@ const Hero = () => {
               className="flex justify-center w-[90%] md:w-fit"
               rel="noopener noreferrer">
               <button className="items-center min-w-[188px] text-base lg:text-xl w-[90%] whitespace-nowrap  justify-center flex hover:gap-4 gap-2 sm:w-fit rounded-2xl bg-[#1F2123] hover:bg-[#36393c] duration-300 ease-in-out transition-all text-white py-3 px-5 h-full">
-                Learn more <svg
+                Learn more{" "}
+                <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -211,7 +219,7 @@ const Hero = () => {
           </div>
           <div className="flex flex-row items-center justify-center gap-4">
             <motion.a
-            className="rounded-xl flex items-center justify-center bg-[#1F2123] hover:bg-[#36393c] duration-300 ease-in-out transition-all p-3"
+              className="rounded-xl flex items-center justify-center bg-[#1F2123] hover:bg-[#36393c] duration-300 ease-in-out transition-all p-3"
               href="https://t.me/LPSwapBot"
               target="_blank"
               rel="noopener noreferrer">
@@ -224,7 +232,7 @@ const Hero = () => {
               />
             </motion.a>
             <motion.a
-            className="rounded-xl flex items-center justify-center bg-[#1F2123] hover:bg-[#36393c] duration-300 ease-in-out transition-all p-3"
+              className="rounded-xl flex items-center justify-center bg-[#1F2123] hover:bg-[#36393c] duration-300 ease-in-out transition-all p-3"
               href="https://x.com/0xLiquiditySwap"
               target="_blank"
               rel="noopener noreferrer">
@@ -237,7 +245,7 @@ const Hero = () => {
               />
             </motion.a>
           </div>
-          
+
           <div className="w-full relative pointer-events-none -mt-5 md:-mt-20">
             <LinesTop />
             <div className="absolute flex justify-center items-center top-0 left-0 w-full h-full z-1">
@@ -252,8 +260,6 @@ const Hero = () => {
               </div>
             </div>
           </div>
-
-          
         </div>
       </div>
     </div>
