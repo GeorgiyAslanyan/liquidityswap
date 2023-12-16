e83e9a9bd3e20622d3994367306d3b2013844ff5
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-12-17 00:24:45 +0500

    add example

---
+++ app/example/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative overflow-hidden bg-black">
+        <div className="absolute opacity-50 pointer-events-none bg-[url('/clearbg.png')] bg-234 md:bg-contain bg-repeat z-[6] w-full h-full "></div>
+        <object className="z-[10] relative" type="image/svg+xml" data="/scheme.svg"></object>
+    </main>
+  );
+}
