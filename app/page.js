f3c1a6232b91022a694fc6cb315b2f0b2ac6abcc
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-12-20 11:42:25 +0500

    add google analytics

--- app/page.js
+++ app/page.js
@@ -1,10 +1,20 @@
 import Description from "@/components/Description";
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import Script from 'next/script'
+
 
 export default function Home() {
   return (
     <main className=" overflow-hidden ">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-1QQESD1ELR"></Script>
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-1QQESD1ELR');`}
+      </Script>
       <Hero />
       <Description />
     </main>
